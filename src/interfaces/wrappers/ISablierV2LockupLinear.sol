bb156ccfa58921f984b994bdc2ec7daff00f1720
mfer1318 <filipeepv@gmail.com>
2023-12-26 20:27:14 +0000

    adding tests: some still failing

--- src/interfaces/wrappers/ISablierV2LockupLinear.sol
+++ src/interfaces/wrappers/ISablierV2LockupLinear.sol
@@ -54,6 +54,44 @@ interface ISablierV2LockupLinear {
         Amounts amounts;
     }
 
+    /// @notice Struct encapsulating the cliff duration and the total duration.
+    /// @param cliff The cliff duration in seconds.
+    /// @param total The total duration in seconds.
+    struct Durations {
+        uint40 cliff;
+        uint40 total;
+    }
+
+    /// @notice Struct encapsulating the broker parameters passed to the create functions. Both can be set to zero.
+    /// @param account The address receiving the broker's fee.
+    /// @param fee The broker's percentage fee from the total amount, denoted as a fixed-point number where 1e18 is 100%.
+    struct Broker {
+        address account;
+        uint256 fee; //UD60x18
+    }
+
+    /// @notice Struct encapsulating the parameters for the {SablierV2LockupLinear.createWithDurations} function.
+    /// @param sender The address streaming the assets, with the ability to cancel the stream. It doesn't have to be the
+    /// same as `msg.sender`.
+    /// @param recipient The address receiving the assets.
+    /// @param totalAmount The total amount of ERC-20 assets to be paid, including the stream deposit and any potential
+    /// fees, all denoted in units of the asset's decimals.
+    /// @param asset The contract address of the ERC-20 asset used for streaming.
+    /// @param cancelable Indicates if the stream is cancelable.
+    /// @param transferable Indicates if the stream NFT is transferable.
+    /// @param durations Struct containing (i) cliff period duration and (ii) total stream duration, both in seconds.
+    /// @param broker Struct containing (i) the address of the broker assisting in creating the stream, and (ii) the
+    /// percentage fee paid to the broker from `totalAmount`, denoted as a fixed-point number. Both can be set to zero.
+    struct CreateWithDurations {
+        address sender;
+        address recipient;
+        uint128 totalAmount;
+        IERC20 asset;
+        bool cancelable;
+        Durations durations;
+        Broker broker;
+    }
+
     /*//////////////////////////////////////////////////////////////
                             ERC721 COMPATIBLE
     //////////////////////////////////////////////////////////////*/
@@ -68,8 +106,14 @@ interface ISablierV2LockupLinear {
     //////////////////////////////////////////////////////////////*/
     function getAsset(uint256 streamId) external view returns (IERC20 asset);
     function getStream(uint256 streamId) external view returns (Stream memory stream);
+
     function isCancelable(uint256 streamId) external view returns (bool result);
     function isStream(uint256 streamId) external view returns (bool result);
+    
+    function createWithDurations(CreateWithDurations calldata params)
+        external
+        returns (uint256 streamId);
+
     function withdrawableAmountOf(uint256 streamId) external view returns (uint128 withdrawableAmount);
     function withdraw(uint256 streamId, address to, uint128 amount) external;
     function withdrawMax(uint256 streamId, address to) external;
--- test/USablierLockupLinearTest.t.sol
+++ test/USablierLockupLinearTest.t.sol
@@ -12,6 +12,8 @@ import {ISablierV2LockupLinear} from '../src/interfaces/wrappers/ISablierV2Locku
 import {UUPSUpgradeable} from '@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol';
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
+import {IWETH} from './test-utils/mock/asset/IWETH.sol';
+
 contract USablierLockupLinearTest is Setup {
   uint256 internal constant ACTOR = 1;
 
@@ -20,15 +22,13 @@ contract USablierLockupLinearTest is Setup {
   USablierLockupLinear sablierLockUp;
   ISablierV2LockupLinear sablier = ISablierV2LockupLinear(0xd4300c5bC0B9e27c73eBAbDc747ba990B1B570Db);
   
-
   function setUp() public virtual override {
     super.setUpByChain(11155111, 4783334);
-    Unlockd unlockd = super.getUnlockd();
     vm.startPrank(_admin);
     _aclManager.setProtocol(makeAddr('protocol'));
 
     USablierLockupLinear implementation = new USablierLockupLinear(address(sablier));
-    address proxyAddress = address(
+    address _proxyAddress = address(
       new ERC1967Proxy(
         address(implementation), 
         abi.encodeWithSelector(
@@ -43,12 +43,59 @@ contract USablierLockupLinearTest is Setup {
         )
       );
 
-    sablierLockUp = USablierLockupLinear(proxyAddress);
+    sablierLockUp = USablierLockupLinear(address(_proxyAddress));
     vm.stopPrank();
   }
 
   function testInitialization() public {
     assertEq(sablierLockUp._wethAddress(), _wethAddress, "WETH address mismatch");
     assertEq(sablierLockUp._usdcAddress(), _usdcAddress, "USDC address mismatch");
+  }
+
+  function testMintReverts() public {
+    vm.prank(address(2)); // Simulate a call from an unauthorized address
+    vm.expectRevert();
+    sablierLockUp.mint(address(0), 1);
+  }
+
+  // function testUpgrade() public {
+  //  USablierLockupLinear newImplementation = new USablierLockupLinear(address(sablier));
+  //  UUPSUpgradeable(address(sablierLockUp)).upgradeTo(address(newImplementation));
+  //  assertEq(address(UUPSUpgradeable(address(sablierLockUp))._getImplementation()), address(newImplementation), "Implementation should be updated");
+  // }
+
+  function testMint() public {
+    // Simulate a call from an authorized address
+    mintSablierNFT();
+    sablierLockUp.mint(address(2), 1);
+    assertEq(sablierLockUp.balanceOf(address(1)), 1, "Balance should be 1");
+  }
+
+  function mintSablierNFT() public {
+     // Simulate a call from an authorized address
+    IERC20(_wethAddress).approve(address(sablier), 2 ether);
+    
+    ISablierV2LockupLinear.Durations memory duration = ISablierV2LockupLinear.Durations({
+      cliff: 1701375965,
+      total: 1701529208
+    });
+
+    ISablierV2LockupLinear.Broker memory broker = ISablierV2LockupLinear.Broker({
+      fee: 0,
+      account: address(0)
+    });
+
+    ISablierV2LockupLinear.CreateWithDurations memory create = ISablierV2LockupLinear.CreateWithDurations({
+        sender: address(1),
+        recipient: address(2),
+        totalAmount: 1 ether,
+        asset: IERC20(_wethAddress),
+        cancelable: false,
+        durations: duration,
+        broker: broker
+    });
+
+    sablier.createWithDurations(create);
+
   }
 }
