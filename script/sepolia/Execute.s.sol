3397273471fc1cd9e84984a93a7ba2db1cb11710
damarnez <dani.elnota@gmail.com>
2024-06-27 17:38:35 +0200

    update

--- script/sepolia/Execute.s.sol
+++ script/sepolia/Execute.s.sol
@@ -7,6 +7,8 @@ import {console} from 'forge-std/console.sol';
 import {DeployConfig} from '../helpers/DeployConfig.sepolia.sol';
 import {ACLManager} from '../../src/libraries/configuration/ACLManager.sol';
 
+import {Constants} from '../../src/libraries/helpers/Constants.sol';
+import {Unlockd} from '../../src/protocol/Unlockd.sol';
 import {Action} from '../../src/protocol/modules/Action.sol';
 import {Auction} from '../../src/protocol/modules/Auction.sol';
 import {BuyNow} from '../../src//protocol/modules/BuyNow.sol';
@@ -16,22 +18,25 @@ import {Market} from '../../src/protocol/modules/Market.sol';
 
 contract ExecuteScript is DeployerHelper {
   function run() external broadcast onlyInChain(DeployConfig.CHAINID) {
-    // Addresses memory addresses = _decodeJson();
-
-    // Manager manager = Manager(0x9151329be2D23fdf33865cA69cf57a727fAC7706);
-
+    Addresses memory addresses = _decodeJson();
+    // ENABLE ADAPTER AND WRAPPER
+    address managerAddress = Unlockd(addresses.unlockd).moduleIdToProxy(
+      Constants.MODULEID__MANAGER
+    );
+    Manager manager = Manager(managerAddress);
+    manager.allowCollectionReserveType(0x388043e55a388e07A75E9A1412FE2d64e48343A5, Constants.ReserveType.ALL);
     // manager.emergencyActivateLoan(
     //   0x0c55e62e379946b598dc89288aae8cffe3027561c0810021286b9620684c9dff
     // );
 
-    Auction auction = Auction(0x876B57F8C3cb8085502cFBF0E47Ca8130Ea6c021);
-    uint256 result = auction.getMinBidPriceAuction(
-      0x0c55e62e379946b598dc89288aae8cffe3027561c0810021286b9620684c9dff,
-      0x4399b219a932066b478a6f69a79412652cf47734f64512b2895af8afaed5880f,
-      1000,
-      0,
-      0
-    );
-    console.log('RESULT', result);
+    // Auction auction = Auction(0x876B57F8C3cb8085502cFBF0E47Ca8130Ea6c021);
+    // uint256 result = auction.getMinBidPriceAuction(
+    //   0x0c55e62e379946b598dc89288aae8cffe3027561c0810021286b9620684c9dff,
+    //   0x4399b219a932066b478a6f69a79412652cf47734f64512b2895af8afaed5880f,
+    //   1000,
+    //   0,
+    //   0
+    // );
+    // console.log('RESULT', result);
   }
 }
